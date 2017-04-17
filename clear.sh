c130561495f59e2cc86a130f2323255fe051f366
maxtom <hitmaxtom@gmail.com>
2017-04-18 07:32:56 +0800

    test loam on dcgan

---
+++ clear.sh
@@ -0,0 +1,3 @@
+rm *~ */*~ */*/*~
+rm *.pyc */*.pyc */*/*.pyc
+clear
--- main.py
+++ main.py
@@ -21,18 +21,18 @@ Usage : see README.md
 """
 
 flags = tf.app.flags
-flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
+flags.DEFINE_integer("epoch", 10000, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The number of batch images [64]")
-flags.DEFINE_integer("image_size", 108, "The size of image to use (will be center cropped) [108]")
+flags.DEFINE_integer("image_size", 500, "The size of image to use (will be center cropped) [108]")
 flags.DEFINE_integer("output_size", 64, "The size of the output images to produce [64]")
 flags.DEFINE_integer("sample_size", 64, "The number of sample images [64]")
 flags.DEFINE_integer("c_dim", 3, "Dimension of image color. [3]")
-flags.DEFINE_integer("sample_step", 500, "The interval of generating sample. [500]")
+flags.DEFINE_integer("sample_step", 50, "The interval of generating sample. [500]")
 flags.DEFINE_integer("save_step", 500, "The interval of saveing checkpoints. [500]")
-flags.DEFINE_string("dataset", "celebA", "The name of dataset [celebA, mnist, lsun]")
+flags.DEFINE_string("dataset", "loam", "The name of dataset [celebA, mnist, loam, lsun]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
 flags.DEFINE_boolean("is_train", False, "True for training, False for testing [False]")
@@ -88,7 +88,11 @@ def main(_):
         g_optim = tf.train.AdamOptimizer(FLAGS.learning_rate, beta1=FLAGS.beta1) \
                           .minimize(g_loss, var_list=g_vars)
 
-    sess = tf.InteractiveSession()
+    # Limit GPU usage
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+    config = tf.ConfigProto(gpu_options=gpu_options)
+    config.gpu_options.allow_growth=True
+    sess = tf.InteractiveSession(config=config)
     tl.layers.initialize_global_variables(sess)
 
     model_dir = "%s_%s_%s" % (FLAGS.dataset, FLAGS.batch_size, FLAGS.output_size)
@@ -115,6 +119,7 @@ def main(_):
         sample = [get_image(sample_file, FLAGS.image_size, is_crop=FLAGS.is_crop, resize_w=FLAGS.output_size, is_grayscale = 0) for sample_file in sample_files]
         sample_images = np.array(sample).astype(np.float32)
         print("[*] Sample images updated!")
+        print sample_images.shape
 
         ## load image data
         batch_idxs = min(len(data_files), FLAGS.train_size) // FLAGS.batch_size
@@ -139,6 +144,7 @@ def main(_):
             sys.stdout.flush()
 
             iter_counter += 1
+            print iter_counter
             if np.mod(iter_counter, FLAGS.sample_step) == 0:
                 # generate and visualize generated images
                 img, errD, errG = sess.run([net_g2.outputs, d_loss, g_loss], feed_dict={z : sample_seed, real_images: sample_images})
