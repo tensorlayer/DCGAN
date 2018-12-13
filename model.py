4bf2bd31b450d77090c0eaf5315b295dd5f9a3fe
Hao <dhsig552@163.com>
2018-12-13 14:57:23 +0800

    Update model.py

--- model.py
+++ model.py
@@ -54,26 +54,26 @@ def discriminator(inputs, is_train=True, reuse=False):
     df_dim = 64   # Dimension of discrim filters in first conv layer. [64]
     w_init = tf.glorot_normal_initializer()
     gamma_init = tf.random_normal_initializer(1., 0.02)
-
+    lrelu = lambda x : tf.nn.leaky_relu(x, 0.2)
     with tf.variable_scope("discriminator", reuse=reuse):
-
+        
         net_in = InputLayer(inputs, name='d/in')
-        net_h0 = Conv2d(net_in, df_dim, (5, 5), (2, 2), act=tf.nn.leaky_relu,
+        net_h0 = Conv2d(net_in, df_dim, (5, 5), (2, 2), act=lrelu,
                 padding='SAME', W_init=w_init, name='d/h0/conv2d')
 
         net_h1 = Conv2d(net_h0, df_dim*2, (5, 5), (2, 2), act=None,
                 padding='SAME', W_init=w_init, name='d/h1/conv2d')
-        net_h1 = BatchNormLayer(net_h1, act=tf.nn.leaky_relu,
+        net_h1 = BatchNormLayer(net_h1, act=lrelu,
                 is_train=is_train, gamma_init=gamma_init, name='d/h1/batch_norm')
 
         net_h2 = Conv2d(net_h1, df_dim*4, (5, 5), (2, 2), act=None,
                 padding='SAME', W_init=w_init, name='d/h2/conv2d')
-        net_h2 = BatchNormLayer(net_h2, act=tf.nn.leaky_relu,
+        net_h2 = BatchNormLayer(net_h2, act=lrelu,
                 is_train=is_train, gamma_init=gamma_init, name='d/h2/batch_norm')
 
         net_h3 = Conv2d(net_h2, df_dim*8, (5, 5), (2, 2), act=None,
                 padding='SAME', W_init=w_init, name='d/h3/conv2d')
-        net_h3 = BatchNormLayer(net_h3, act=tf.nn.leaky_relu,
+        net_h3 = BatchNormLayer(net_h3, act=lrelu,
                 is_train=is_train, gamma_init=gamma_init, name='d/h3/batch_norm')
 
         net_h4 = FlattenLayer(net_h3, name='d/h4/flatten')
