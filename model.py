166e08d098d2fa434ba3184c30f4a3004924c965
zsdonghao <dhsig552@163.com>
2019-03-16 16:03:13 +0800

    update model API

--- model.py
+++ model.py
@@ -14,20 +14,20 @@ def get_generator(shape):
     gamma_init = tf.random_normal_initializer(1., 0.02)
 
     ni = Input(shape)
-    nn = Dense(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init)(ni)
+    nn = Dense(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init, b_init=None)(ni)
     nn = Reshape(shape=[-1, s16, s16, gf_dim*8])(nn)
     nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
 
-    nn = DeConv2d(gf_dim * 4, (5, 5), strides=(2, 2), padding='SAME', W_init=w_init)(nn)
+    nn = DeConv2d(gf_dim * 4, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d( decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
     #
-    nn = DeConv2d(gf_dim * 2, (5, 5), strides=(2, 2), padding='SAME', W_init=w_init)(nn)
+    nn = DeConv2d(gf_dim * 2, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
     #
-    nn = DeConv2d(gf_dim, (5, 5), strides=(2, 2), padding='SAME', W_init=w_init)(nn)
+    nn = DeConv2d(gf_dim, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
     #
-    nn = DeConv2d(c_dim, (5, 5), strides=(2, 2), padding='SAME', act=tf.nn.tanh, W_init=w_init)(nn)
+    nn = DeConv2d(c_dim, (5, 5), (2, 2), act=tf.nn.tanh, W_init=w_init)(nn)
 
     return tl.models.Model(inputs=ni, outputs=nn, name='generator')
 
@@ -38,15 +38,15 @@ def get_discriminator(shape):
     lrelu = lambda x : tf.nn.leaky_relu(x, 0.2)
 
     ni = Input(shape)
-    nn = Conv2d(df_dim, (5, 5), (2, 2), act=lrelu, padding='SAME', W_init=w_init)(ni)
+    nn = Conv2d(df_dim, (5, 5), (2, 2), act=lrelu, W_init=w_init)(ni)
 
-    nn = Conv2d(df_dim*2, (5, 5), (2, 2), padding='SAME', W_init=w_init)(nn)
+    nn = Conv2d(df_dim*2, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(nn)
 
-    nn = Conv2d(df_dim*4, (5, 5), (2, 2), padding='SAME', W_init=w_init)(nn)
+    nn = Conv2d(df_dim*4, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(nn)
 
-    nn = Conv2d(df_dim*8, (5, 5), (2, 2), padding='SAME', W_init=w_init)(nn)
+    nn = Conv2d(df_dim*8, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(nn)
 
     nn = Flatten()(nn)
