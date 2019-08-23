0c5d6c9e4fdd827cbff64a11d7efd471ce97cc06
Hao <dhsig552@163.com>
2019-08-23 20:57:41 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorlayer as tl
-from tensorlayer.layers import Input, Dense, DeConv2d, Reshape, BatchNorm2d, Conv2d, Flatten, BatchNorm
+from tensorlayer.layers import Input, Dense, DeConv2d, Reshape, BatchNorm2d, Conv2d, Flatten
 
 def get_generator(shape, gf_dim=64): # Dimension of gen filters in first conv layer. [64]
     image_size = 64
@@ -12,7 +12,7 @@ def get_generator(shape, gf_dim=64): # Dimension of gen filters in first conv la
     ni = Input(shape)
     nn = Dense(n_units=(gf_dim * 8 * s16 * s16), W_init=w_init, b_init=None)(ni)
     nn = Reshape(shape=[-1, s16, s16, gf_dim*8])(nn)
-    nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
+    nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
     nn = DeConv2d(gf_dim * 4, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
     nn = BatchNorm2d( decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
     nn = DeConv2d(gf_dim * 2, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn)
